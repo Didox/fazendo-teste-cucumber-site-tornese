a770f18c46c2591470d2b6c44a439d29d50b408a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-03-23 22:38:10 -0300

    refatorando com objeto melhorando metodos

--- src/test/java/io/cucumber/danilo/BuscandoConteudoDeCacheSteps.java
+++ src/test/java/io/cucumber/danilo/BuscandoConteudoDeCacheSteps.java
@@ -5,12 +5,12 @@ import io.cucumber.java.pt.*;
 
 public class BuscandoConteudoDeCacheSteps {
   @Dada("cliquei no curso de cache")
-  public void cliquei_no_curso_de_cache() {
+  public void clicarNoCursoDeCache() {
     Configuracao.seletorQueryCss("a[href='/aulas/cache'] img").click();
   }
 
   @Entao("devo clicar no curso de varnish para poder assistir")
-  public void devo_clicar_no_curso_de_varnish_para_poder_assistir() {
+  public void clicarNoCacheVarnish() {
     Configuracao.seletorQueryCss(".div-card-aulas:last-child").click();
     Configuracao.fechar();
   }
